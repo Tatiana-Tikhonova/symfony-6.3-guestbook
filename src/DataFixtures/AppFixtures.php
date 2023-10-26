e8d18be9e1cbac77c8fe4ba533d10b105d9ba135
Tatiana-Tikhonova <tantikh2020@gmail.com>
2023-10-26 11:42:10 +0300

    install messenger

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -37,7 +37,15 @@ class AppFixtures extends Fixture
         $comment1->setAuthor('Kelly');
         $comment1->setEmail('kelly@mail.com');
         $comment1->setText('This was a great conference!');
+        $comment1->setState('published');
         $manager->persist($comment1);
+
+        $comment2 = new Comment();
+        $comment2->setConference($moskva);
+        $comment2->setAuthor('Lucas');
+        $comment2->setEmail('lucas@mail.ru');
+        $comment2->setText('Thank`s!');
+        $manager->persist($comment2);
 
         $admin = new Admin();
         $admin->setRoles(['ROLE_ADMIN']);
